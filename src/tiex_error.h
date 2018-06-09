8ca112715fd59d263e2cd2abb9e66f028d894f03
Zplutor <zplutor@qq.com>
2018-06-09 23:40:32 +0800

    Add documentation.

--- src/tiex_error.h
+++ src/tiex_error.h
@@ -4,16 +4,43 @@
 
 namespace tiex {
 
+/**
+ Represents an error when parsing format expression.
+ */
 class ParseError {
 public:
+    /**
+     Describes the reason of parse error.
+     */
     enum class Status {
+        
+        /**
+         No error, means that parsing is succeeded.
+         */
         None,
+        
+        /**
+         The format expression ends unexpectedly.
+         */
         UnexpectedEnd,
+        
+        /**
+         An unexpected token is met.
+         
+         Typically this means a syntax error.
+         */
         UnexpectedToken,
+        
+        /**
+         The number in expression is fail to convert to numerical value.
+         */
         ConversionFailed,
     };
     
 public:
+    /**
+     Clear error information, reset to default state.
+     */
     void Clear() {
         status = Status::None;
         index = 0;
@@ -21,26 +48,61 @@ public:
     }
     
 public:
+    /**
+     Status of the error.
+     */
     Status status = Status::None;
+    
+    /**
+     The parsing position in format expression when the error occurs.
+     */
     std::size_t index = 0;
+    
+    /**
+     The token that causes the error. Maybe empty.
+     */
     String token;
 };
 
 
+/**
+ Represents an error when formatting times.
+ */
 class FormatError {
 public:
+    /**
+     Describes the reason of format error.
+     */
     enum class Status {
+        
+        /**
+         No error, means that formatting is succeeded.
+         */
         None,
+        
+        /**
+         No rule that matches the relationship of times.
+         */
         NoMatchedRule,
+        
+        /**
+         A general status that represents errors related to time conversion.
+         */
         TimeError,
     };
     
 public:
+    /**
+     Clear error information, reset to default state.
+     */
     void Clear() {
         status = Status::None;
     }
     
 public:
+    /**
+     Status of the error.
+     */
     Status status = Status::None;
 };
     
--- src/tiex_formatter.h
+++ src/tiex_formatter.h
@@ -8,24 +8,131 @@
 
 namespace tiex {
 
+/**
+ A formatter represents a format expression that has been parsed,
+ and is used to format relative times to string by the meaning of
+ the expression.
+ 
+ Typically, to create a formmater, you should write a format expression as
+ string and pass it to the static function Create. You should reuse one
+ formatter whenever possible if the same expression is needed for multiple
+ times, to avoid re-parsing and performance.
+ */
 class Formatter {
 public:
+    /**
+     Create a formatter.
+     
+     @param string
+       An expression string that to be parsed.
+     
+     @return
+       An empty formatter if fail to parse the expression.
+     */
     static Formatter Create(const String& string);
+    
+    /**
+     Create a formatter and catch parse error.
+     
+     @param string
+       An expression string that to be parsed.
+     
+     @param parse_error
+       An output parameter that stores information about parse error.
+     
+     @return
+       An empty formatter if fail to parse the expression.
+    */
     static Formatter Create(const String& string, ParseError& parse_error);
     
 public:
+    /**
+     Construct an empty formatter.
+     */
     Formatter();
+    
+    /**
+     Construct a formatter with an expression, by copying its content.
+     */
     Formatter(const Expression& expression);
+    
+    /**
+     Construct a formatter with an expression, by moving its content.
+     */
     Formatter(Expression&& expression);
     
-    String Format(std::time_t referenced_time, std::time_t formatted_time, const Locale& locale, FormatError& format_error);
+    /**
+     Format times with locale information and catch format error.
+     
+     @param referenced_time
+       The referenced time that is used to compare to the formatted time.
+     
+     @param formatted_time
+       The target time to be formatted to string.
+     
+     @param locale
+       Contains localization information that affect format result.
+     
+     @param format_error
+       An output parameter that stores information about format error.
+     
+     @return
+       A format result string. An empty string is returned if fail to format.
+     */
+    String Format(
+        std::time_t referenced_time,
+        std::time_t formatted_time,
+        const Locale& locale,
+        FormatError& format_error);
     
+    /**
+     Format times and catch format error.
+     
+     @param referenced_time
+       The referenced time that is used to compare to the formatted time.
+     
+     @param formatted_time
+       The target time to be formatted to string.
+     
+     @param format_error
+       An output parameter that stores information about format error.
+     
+     @return
+       A format result string. An empty string is returned if fail to format.
+     */
     String Format(std::time_t referenced_time, std::time_t formatted_time, FormatError& format_error) {
         return Format(referenced_time, formatted_time, Locale(), format_error);
     }
     
+    /**
+     Format times with locale information.
+     
+     @param referenced_time
+       The referenced time that is used to compare to the formatted time.
+     
+     @param formatted_time
+       The target time to be formatted to string.
+     
+     @param locale
+       Contains localization information that affect format result.
+     
+     @return
+       A format result string. An empty string is returned if fail to format.
+     */
     String Format(std::time_t referenced_time, std::time_t formatted_time, const Locale& locale);
     
+    /**
+     Format times.
+     
+     @param referenced_time
+       The referenced time that is used to compare to the formatted time.
+     
+     @param formatted_time
+       The target time to be formatted to string.
+     
+     @return
+       A format result string. An empty string is returned if fail to format.
+     */
     String Format(std::time_t referenced_time, std::time_t formatted_time) {
         return Format(referenced_time, formatted_time, Locale());
     }
