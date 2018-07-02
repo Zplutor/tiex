c6d369bcdcf80aecc3884c7e6358aa12998bbd23
Zplutor <zplutor@qq.com>
2018-07-02 15:20:23 +0800

    Add documentation.

--- src/tiex_locale.h
+++ src/tiex_locale.h
@@ -10,9 +10,23 @@ namespace tiex {
  */
 class Locale {
 public:
+    /**
+     Contains options to format localized string of month.
+     */
     class MonthOptions {
     public:
+        /**
+         Indicates that whether a text of number is expected.
+         
+         If it is true, corresponds to %m specifier, otherwise corresponds to %b, %B or %h specifiers.
+         */
         bool is_number = false;
+        
+        /**
+         Indicates that whether an abbreviated result is expected.
+         
+         If it is true, corresponds to %b or %h specifiers, otherwise corresponds to %B specifier.
+         */
         bool is_abbreviated = false;
     };
     
@@ -31,8 +45,16 @@ public:
      */
     typedef std::function<String(int month, const MonthOptions& options)> GetMonthCallback;
     
+    /**
+     Contains options to format localized string of weekday.
+     */
     class WeekdayOptions {
     public:
+        /**
+         Indicates that whether an abbreviated result is expected.
+         
+         If it is true, corresponds to %a specifier, otherwise corresponds to %A specifier.
+         */
         bool is_abbreviated = false;
     };
     
@@ -62,8 +84,16 @@ public:
      */
     typedef std::function<String(bool is_pm)> GetAmPmCallback;
     
+    /**
+     Contains options to format localized string of hour.
+     */
     class HourOptions {
     public:
+        /**
+         Indicates that whether the hour is based on 12 hour clock.
+         
+         If it is true, corresponds to %I specifier, otherwise corresponds to %H specifier.
+         */
         bool is_12_hour_clock = false;
     };
     
@@ -71,7 +101,7 @@ public:
      The prototype of callback to get localized string of hour.
      
      @param hour
-       Numerical value of hour, from 0 to 23.
+       Numerical value of hour, from 0 to 23 on 24 hour clock, from 1 to 12 on 12 hour clock.
      
      @param options
        Format options. An implementation of the callback should format
@@ -81,6 +111,28 @@ public:
        A string of hour after formatted.
      */
     typedef std::function<String(int hour, const HourOptions& options)> GetHourCallback;
+    
+    /**
+     The prototype of callback to get localized string of minute.
+     
+     @param minute
+       Numerical value of minute, from 0 to 59.
+     
+     @return
+       A string of minute after formatted.
+     */
+    typedef std::function<String(int minute)> GetMinuteCallback;
+    
+    /**
+     The prototype of callback to get localized string of second.
+     
+     @param second
+       Numerical value of second, from 0 to 59.
+     
+     @return
+       A string of second after formatted.
+     */
+    typedef std::function<String(int second)> GetSecondCallback;
     
 public:
     /**
@@ -106,12 +158,12 @@ public:
     /**
      A callback to get localized string of minute.
      */
-    std::function<String(int minute)> get_minute;
+    GetMinuteCallback get_minute;
     
     /**
      A callback to get localized string of second.
      */
-    std::function<String(int second)> get_second;
+    GetSecondCallback get_second;
 };
     
 }
